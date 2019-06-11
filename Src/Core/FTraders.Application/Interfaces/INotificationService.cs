9a5fed389b0930f250be17b9eec92828cb41bc4a
Gurjeet Sasan <developer.gurjeet@gmail.com>
2019-06-11 16:15:45 +0530

    Interfaces

---
+++ Src/Core/FTraders.Application/Interfaces/INotificationService.cs
@@ -0,0 +1,10 @@
+﻿using FTraders.Application.Notifications.Models;
+using System.Threading.Tasks;
+
+namespace FTraders.Application.Interfaces
+{
+    public interface INotificationService
+    {
+        Task SendAsync(Message message);
+    }
+}
