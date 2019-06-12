889bae5c5ae43c805a71756f92f241ad3a32196d
Gurjeet Sasan <developer.gurjeet@gmail.com>
2019-06-12 12:37:49 +0530

    Completed Datatime Interface

---
+++ Src/Common/FTraders.Common/IDateTime.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FTraders.Common
+{
+    public interface IDateTime
+    {
+        DateTime Now { get; }
+    }
+}
